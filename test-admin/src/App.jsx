602ad32ab793c2e06a8b36672c4c690daba244d8
Alexander Vetrov <notescape262@gmail.com>
2023-01-26 18:15:36 +0200

    working test

---
+++ test-admin/src/App.jsx
@@ -0,0 +1,15 @@
+import { Admin, Resource, ListGuesser } from 'react-admin'
+import jsonServerProvider from 'ra-data-json-server'
+import './App.css'
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
+
+const App = () => {
+  return (
+    <Admin dataProvider={dataProvider}>
+      <Resource name="users" list={ListGuesser} />
+    </Admin>
+  )
+}
+
+export default App
