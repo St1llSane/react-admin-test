bd5c60a71479be1fefdd04bf14c3c5cf2fffb0e4
Alexander Vetrov <notescape262@gmail.com>
2023-01-26 22:08:31 +0200

    authProvider

--- test-admin/src/App.jsx
+++ test-admin/src/App.jsx
@@ -2,15 +2,30 @@ import { Admin, Resource } from 'react-admin'
 import jsonServerProvider from 'ra-data-json-server'
 import UserList from './components/UserList'
 import { Posts, PostsCreate, PostsEdit } from './components/Posts'
+import UserIcon from '@mui/icons-material/Group'
+import PostIcon from '@mui/icons-material/Book'
+import { Dashboard } from './components/Dashboard'
+import { authProvider } from './authProvider'
 import './App.css'
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 
 const App = () => {
   return (
-    <Admin dataProvider={dataProvider}>
-      <Resource name="users" list={UserList} recordRepresentation="name" />
-      <Resource name="posts" list={Posts} edit={PostsEdit} create={PostsCreate} />
+    <Admin authProvider={authProvider} dataProvider={dataProvider} dashboard={Dashboard}>
+      <Resource
+        name="users"
+        list={UserList}
+        recordRepresentation="name"
+        icon={UserIcon}
+      />
+      <Resource
+        name="posts"
+        list={Posts}
+        edit={PostsEdit}
+        create={PostsCreate}
+        icon={PostIcon}
+      />
     </Admin>
   )
 }
---
+++ test-admin/src/components/Dashboard.jsx
@@ -0,0 +1,8 @@
+import { Card, CardContent, CardHeader } from '@mui/material'
+
+export const Dashboard = () => (
+  <Card sx={{ mt: 2 }}>
+    <CardHeader title="Welcome to the administration" />
+    <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+  </Card>
+)
