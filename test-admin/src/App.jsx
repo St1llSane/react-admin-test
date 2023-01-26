cf52271d8e3d46d449dfc98e365a5fd78fc9ef51
Alexander Vetrov <notescape262@gmail.com>
2023-01-26 18:42:50 +0200

    data from custom component

--- test-admin/src/App.jsx
+++ test-admin/src/App.jsx
@@ -1,5 +1,6 @@
-import { Admin, Resource, ListGuesser } from 'react-admin'
+import { Admin, Resource } from 'react-admin'
 import jsonServerProvider from 'ra-data-json-server'
+import UserList from './components/UserList'
 import './App.css'
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
@@ -7,7 +8,7 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 const App = () => {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="users" list={ListGuesser} />
+      <Resource name="users" list={UserList} />
     </Admin>
   )
 }
---
+++ test-admin/src/components/UserList.jsx
@@ -0,0 +1,20 @@
+import { Datagrid, EmailField, List, TextField } from 'react-admin'
+
+const UserList = () => {
+  return (
+    <List>
+      <Datagrid rowClick="edit">
+        <TextField source="id" />
+        <TextField source="name" />
+        <TextField source="userName" />
+        <EmailField source="email" />
+        <TextField source="address.street" />
+        <TextField source="phone" />
+        <TextField source="website" />
+        <TextField source="company.name" />
+      </Datagrid>
+    </List>
+  )
+}
+
+export default UserList
