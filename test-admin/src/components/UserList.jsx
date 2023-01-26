336ee6da4b9d51f37cf18874f2d79af872712d76
Alexander Vetrov <notescape262@gmail.com>
2023-01-26 18:58:24 +0200

    useMediaQuery

--- test-admin/src/components/UserList.jsx
+++ test-admin/src/components/UserList.jsx
@@ -1,13 +1,27 @@
 import { Datagrid, EmailField, List, SimpleList, TextField } from 'react-admin'
+import { useMediaQuery } from '@mui/material'
 
 const UserList = () => {
+  const isSmall = useMediaQuery((theme) => theme.breakpoints.down('sm'))
+
   return (
     <List>
-      <SimpleList
-        primaryText={(record) => record.name}
-        secondaryText={(record) => record.username}
-        tertiaryText={(record) => record.email}
-      />
+      {isSmall ? (
+        <SimpleList
+          primaryText={(record) => record.name}
+          secondaryText={(record) => record.username}
+          tertiaryText={(record) => record.email}
+        />
+      ) : (
+        <Datagrid rowClick="edit">
+          <TextField source="id" />
+          <TextField source="name" />
+          <EmailField source="email" />
+          <TextField source="phone" />
+          <TextField source="website" />
+          <TextField source="company.name" />
+        </Datagrid>
+      )}
     </List>
   )
 }
